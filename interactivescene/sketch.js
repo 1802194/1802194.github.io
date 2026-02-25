af59aef06f20eca76d016dbd9e0bf9a702398446
1802194 <1802194@spsd.sk.ca>
2026-02-25 10:09:27 -0600

    auto fixed warnings

--- interactivescene/sketch.js
+++ interactivescene/sketch.js
@@ -1,22 +1,22 @@
 let circleX;
 let circleY;
-let speedX
-let speedY
+let speedX;
+let speedY;
 let diameter = 50;
-let pos
-let vel
-let accelX = 1
-let accelY = 1
+let pos;
+let vel;
+let accelX = 1;
+let accelY = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-  circleX = windowWidth / 2
-  circleY = windowHeight / 2
-  pos = createVector(circleX, circleY)
-  vel = createVector(0, 0)
-  speedX = 0
-  speedY = 0
+  circleX = windowWidth / 2;
+  circleY = windowHeight / 2;
+  pos = createVector(circleX, circleY);
+  vel = createVector(0, 0);
+  speedX = 0;
+  speedY = 0;
 }
 
 function draw() {
@@ -24,11 +24,11 @@ function draw() {
   
   circleDraw();
   circleMove();
-  vel.y = speedY
-  vel.x = speedX
-  pos.add(vel)
-  friction()
-  speedLimit()
+  vel.y = speedY;
+  vel.x = speedX;
+  pos.add(vel);
+  friction();
+  speedLimit();
   
 }
 
@@ -40,7 +40,7 @@ function circleMove() {
     speedX -= accelX;
   }
   if (keyIsDown(83)) { //s
-    speedY += accelY
+    speedY += accelY;
   }
   if (keyIsDown(68)) { //d
     speedX += accelX;
@@ -48,36 +48,36 @@ function circleMove() {
 }
 
 function circleDraw() {
-  fill("black")
-  circle(pos.x, pos.y, diameter)
+  fill("black");
+  circle(pos.x, pos.y, diameter);
 }
 
 function friction() {
   if (speedX > 0) {
-    speedX -= (accelX / 2)
+    speedX -= accelX / 2;
   }
   if (speedX < 0) {
-    speedX += (accelX / 2)
+    speedX += accelX / 2;
   }
   if (speedY > 0) {
-    speedY -= (accelY / 2)
+    speedY -= accelY / 2;
   }
   if (speedY < 0) {
-    speedY += (accelY / 2)
+    speedY += accelY / 2;
   }
 }
 
 function speedLimit() {
   if (speedX > 10) {
-    speedX -= (accelX / 2)
+    speedX -= accelX / 2;
   }
   if (speedX < -10) {
-    speedX += (accelX / 2)
+    speedX += accelX / 2;
   }
   if (speedY > 10) {
-    speedY -= (accelY / 2)
+    speedY -= accelY / 2;
   }
   if (speedY < -10) {
-    speedY += (accelY / 2)
+    speedY += accelY / 2;
   }
 }
