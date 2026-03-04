a0b9ab006253c62d4552b2b92c2e51de67c4a9d4
1802194 <1802194@spsd.sk.ca>
2026-03-04 11:04:01 -0600

    updated circlearray demo

--- ballarray/sketch.js
+++ ballarray/sketch.js
@@ -13,23 +13,40 @@ function draw() {
     //move
     ball.x += ball.dx;
     ball.y += ball.dy;
+    if (ball.x > windowWidth + ball.radius) {
+      ball.x = 0 - ball.radius;
+    }
+    if (ball.x < 0 - ball.radius) {
+      ball.x = windowWidth + ball.radius;
+    }
+    if (ball.y > windowHeight + ball.radius) {
+      ball.y = 0 - ball.radius;
+    }
+    if (ball.y < 0 - ball.radius) {
+      ball.y = windowHeight + ball.radius;
+    }
 
     //display
+    fill(ball.r,ball.g,ball.b);
     circle(ball.x, ball.y, ball.radius * 2);
   }
 }
 
 function mousePressed() {
-  spawnBall();
+  spawnBall(mouseX, mouseY);
 }
 
-function spawnBall() {
+function spawnBall(_x, _y) {
   let someBall = {
-    x:random(width),
-    y:random(height),
+    x:_x,
+    y:_y,
     dx:random(-5, 5),
     dy:random(-5, 5),
     radius:random(10, 30),
+    r:random(0,255),
+    g:random(0,255),
+    b:random(0,255),
+
   };
   ballArray.push(someBall);
 }
